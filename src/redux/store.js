110b365fc124d78da0f26794ea75756e3e7ed120
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-01-02 22:57:39 +0530

    #update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk"; // Use named import
 import rootReducer from "./reducers/rootReducer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
